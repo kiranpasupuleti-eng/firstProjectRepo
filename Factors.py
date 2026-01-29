4f2914145ecd3d2cc9467ad20bcc3e2b2f1df057
Kiran Pasupuleti <kiranpasupuleti47@gmail.com>
2026-01-29 16:32:22 +0530

    Update Factors.py

--- Factors.py
+++ Factors.py
@@ -1,6 +1,13 @@
 n=int(input("Enter a number:"))
 i=1
-while i<=n:
+s=0
+while i!=n:
   if n%i==0:
     print(i)
+    s+=i
   i+=1
+print("Sum Of Factors is:",s)
+if s==n:
+  print("Perfect Number")
+else:
+  print("Not A Perfect Number")
